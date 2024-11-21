8f78fdb4d8de3baac3ff97aaead9a1029c49a3d3
mdtuhinislam <mdtuhinislam@gmail.com>
2024-11-21 23:42:16 +0600

    added Models and Common in Ordering

---
+++ Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using Ordering.Domain.Common;
+
+namespace Ordering.Domain.Models
+{
+    public class Order : EntityBase
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public Decimal TotalPrice { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+
+
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public string Expiration { get; set; }
+        public int PaymentMethod { get; set; }
+    }
+}
